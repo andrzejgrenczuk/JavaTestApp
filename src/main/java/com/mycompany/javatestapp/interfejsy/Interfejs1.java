2cff547b36a1e0d393f6e2b9712d7038a8161625
Andrzej Greńczuk <andrzej.grenczuk@gmail.com>
2020-08-01 21:33:02 +0200

    rozwój, suczko :)

---
+++ src/main/java/com/mycompany/javatestapp/interfejsy/Interfejs1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javatestapp.interfejsy;
+
+/**
+ *
+ * @author andrz
+ */
+public interface Interfejs1 {
+    public void sss(int x, int y, int z);
+}
